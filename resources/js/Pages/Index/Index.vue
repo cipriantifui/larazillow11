15eeed5f8bf9bd6f3579976e49b8cb59fc782a80
Ciprian Tifui <ciprian.tifui@ipsos.com>
2024-06-15 16:51:56 +0300

    add default layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,16 +1,17 @@
 <template>
    <div>
-        <h1>Index</h1>
-    </div>
-    <Link href="/hello">Show Page</Link>
-    <div>
-        The message is: {{ message }}
-    </div>
+       <h1>Index</h1>
+   </div>
+   <Link href="/hello">Show Page</Link>
+   <div>
+       The message is: {{ message }}
+   </div>
 </template>
 <script setup>
     import {Link} from '@inertiajs/vue3';
-    defineProps(['message']);
+    defineProps({message: String});
 </script>
+
 <style scoped>
 
 </style>
