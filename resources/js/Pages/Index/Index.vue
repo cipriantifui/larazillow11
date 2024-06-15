82f885e738a1d5365af3d76945745d666e66433f
Ciprian Tifui <ciprian.tifui@ipsos.com>
2024-06-15 13:20:18 +0300

    second commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+   <div>
+        <h1>Index</h1>
+    </div>
+    <Link href="/hello">Show Page</Link>
+    <div>
+        The message is: {{ message }}
+    </div>
+</template>
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+    defineProps(['message']);
+</script>
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+    </div>
+    <Link href="/">Main Page</Link>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+</script>
+
+<style scoped>
+
+</style>
