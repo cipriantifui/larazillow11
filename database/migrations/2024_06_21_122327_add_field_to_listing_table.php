59b5b4dc94b9661a8ffbd9e760a30bc41463822f
Ciprian Tifui <ciprian.tifui@ipsos.com>
2024-06-21 15:32:41 +0300

    configure database and docker

---
+++ database/migrations/2024_06_21_122327_add_field_to_listing_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->unsignedTinyInteger('beds')->after('id');
+            $table->unsignedTinyInteger('baths')->after('beds');
+            $table->unsignedTinyInteger('area')->after('baths');
+
+            $table->tinyText('city')->after('area');
+            $table->tinyText('code')->after('city');
+            $table->tinyText('street')->after('code');
+            $table->tinyText('street_nr')->after('street');
+
+            $table->unsignedInteger('price')->after('street_nr');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropColumns('listings', ['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price']);
+    }
+};
