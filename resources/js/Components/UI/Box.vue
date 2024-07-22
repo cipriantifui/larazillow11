ae2806564b794199834507d28f689deb948029b1
Ciprian Tifui <ciprian.tifui@ipsos.com>
2024-07-22 11:19:21 +0300

    refactoring listings style

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="border border-gray-300 dark:border-gray-800 bg-white dark:bg-gray-800 p-4 rounded-md shadow-md dark:text-gray-300">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
