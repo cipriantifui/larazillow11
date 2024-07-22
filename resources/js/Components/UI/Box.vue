4b866e2ff4b4901fd2aa7b5db60c30cc3fe560f2
Ciprian Tifui <ciprian.tifui@ipsos.com>
2024-07-22 12:51:41 +0300

    refactoring show component

--- resources/js/Components/UI/Box.vue
+++ resources/js/Components/UI/Box.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="border border-gray-300 dark:border-gray-800 bg-white dark:bg-gray-800 p-4 rounded-md shadow-md dark:text-gray-300">
+        <div class="text-gray-500 font-medium">
+            <slot name="header"></slot>
+        </div>
         <slot />
     </div>
 </template>
